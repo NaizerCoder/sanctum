38eb1dbd2b9014d5f3b17b9b0a6db504815b4536
PROD <mr.beensir@yandex.ru>
2024-05-22 16:21:52 +0300

    PROD:22.05.2024

--- config/sanctum.php
+++ config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'sanctum, http://sanctum.licab.ru, https://sanctum.licab.ru, localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
+        'sanctum,sanctum.licab.ru,licab.ru,localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
